08ec89418bf3cb2a1f2c4784ca0431b4f5f1133c
FarpadGit <farpadmail@gmail.com>
2025-06-09 22:59:45 +0200

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,11 +1,10 @@
-cd main
-python3 -m pip install -r requirements.txt
+python3 -m pip install -r ./main/requirements.txt
 python3 manage.py collectstatic --noinput
 
-cd ..
 mkdir -p .vercel/output/static
-cp -r ./main/staticfiles/ .vercel/output/static/
+cp -r staticfiles/ .vercel/output/static/
 
 cd main
 python3 manage.py makemigrations
 python3 manage.py migrate
+cd ..

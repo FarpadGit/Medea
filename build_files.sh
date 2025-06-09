ef373674ebc68ef2d070de41b6d27badb82cceb5
FarpadGit <farpadmail@gmail.com>
2025-06-10 00:21:55 +0200

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,6 @@
+echo $PYTHONPATH
 export PYTHONPATH=/python312/lib/python3.12/site-packages:$PYTHONPATH
+echo $PYTHONPATH
 
 python3 -m pip install -r ./main/requirements.txt
 python3 ./main/manage.py collectstatic --noinput

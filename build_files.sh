1bd09a3e6546b2388cb903cf795b01def2ed45af
FarpadGit <farpadmail@gmail.com>
2025-06-10 00:46:44 +0200

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,8 @@
 python3 -m venv venv
 source venv/bin/activate
 
+python3 -m pip install --upgrade pip
+
 python3 -m pip install -r ./main/requirements.txt
 python3 ./main/manage.py collectstatic --noinput
 

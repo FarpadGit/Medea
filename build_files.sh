eb7cc82b8995c0bfb342eb7805e60e9a932ae3e6
FarpadGit <farpadmail@gmail.com>
2025-06-10 01:08:05 +0200

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -6,9 +6,6 @@ python3 -m pip install --upgrade pip
 python3 -m pip install -r ./main/requirements.txt
 python3 ./main/manage.py collectstatic --noinput
 
-pip list -v
-export PYTHONPATH=/vercel/path0/venv/lib/python3.12/site-packages:$PYTHONPATH
-
 mkdir -p .vercel/output/static
 cp -r ./main/staticfiles/* .vercel/output/static/
 

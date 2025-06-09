c591f9fc69bb1dcaaf6a7a983c3f5ee64fe82933
FarpadGit <farpadmail@gmail.com>
2025-06-09 22:08:31 +0200

    updated vercel files

--- build_files.sh
+++ build_files.sh
@@ -1,8 +1,8 @@
-python3.9 -m pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput
+python3 -m pip install -r requirements.txt
+python3 manage.py collectstatic --noinput
 
 mkdir -p .vercel/output/static
 cp -r staticfiles/ .vercel/output/static/
 
-python3.9 manage.py makemigrations
-python3.9 manage.py migrate
+python3 manage.py makemigrations
+python3 manage.py migrate

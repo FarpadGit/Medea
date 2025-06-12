ab3856489b63feba4e1bf90e10c03df6e3363a5f
FarpadGit <farpadmail@gmail.com>
2025-06-12 22:46:25 +0200

    Update utils.py

--- main/files/utils.py
+++ main/files/utils.py
@@ -5,14 +5,13 @@ from .models import Files, ttl
 
 from threading import Timer
 
-def generate_random_string(length):
-    random_string = "".join(random.choices(string.ascii_letters + string.digits, k=length))
-    return random_string
+def random_string(length):
+    return "".join(random.choices(string.ascii_letters + string.digits, k=length))
 
 def generate_database_entry(name: str, content: str):
-    download_id = generate_random_string(6)
+    download_id = random_string(6)
     while Files.objects.filter(download_id = download_id).count() > 0:
-        download_id = generate_random_string(6)
+        download_id = random_string(6)
     
     # convert decrypted data into an HttpRequest body for Django Rest Framework to use and save into db
     save_request = Request(HttpRequest())
